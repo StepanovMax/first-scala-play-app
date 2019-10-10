a61fc6d40eb923dc020d3ef795abbe802c1239de
Stepanov Max <badsanta2010@gmail.com>
2019-10-10 23:01:08 +0300

    Add post request

--- app/controllers/TaskList1.scala
+++ app/controllers/TaskList1.scala
@@ -17,7 +17,18 @@ class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(c
     Ok(views.html.login1())
   }
 
-  def validateLogin1(username: String, password: String) = Action {
+  def validateGET1(username: String, password: String) = Action {
     Ok(s"$username and $password")
+  }
+
+  def validatePOST1() = Action {
+    request =>
+      var postVals = request.body.asFormUrlEncoded
+      postVals.map {
+        args =>
+          val username = args("username").head
+          val password = args("password").head
+          Redirect(routes.TaskList1.taskList())
+      }.getOrElse(Redirect(routes.TaskList1.login1()))
   }
 }
